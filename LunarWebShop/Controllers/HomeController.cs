b18a239db29ed43d9a85dbc4766ad8d76d0cd0d9
Unknown <jeremyvancampen97@gmail.com>
2018-03-18 13:01:23 +0100

    Dashboard toegevoegd voor de admin voor het beheren van producten

--- LunarWebShop/Controllers/HomeController.cs
+++ LunarWebShop/Controllers/HomeController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LunarWebShop.Models;
 
 namespace LunarWebShop.Controllers
 {
     public class HomeController : Controller
     {
+        LunarProduct _database = new LunarProduct();
         public ActionResult Index()
         {
-            return View();
+            return View(_database.Product.ToList());
         }
 
         public ActionResult About()
---
+++ LunarWebShop/Controllers/ProductenController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LunarWebShop.Models;
+
+namespace LunarWebShop.Controllers
+{
+    public class ProductenController : Controller
+    {
+       LunarProduct _database = new LunarProduct();
+        // GET: Producten
+        public ActionResult Product()
+        {
+            return View(_database.Product.ToList());
+        }
+
+        // GET: Producten/Details/5
+        public ActionResult Details(int id)
+        {
+            Product Product = _database.Product.Find(id);
+            return View(Product);
+        }
+
+        // GET: Producten/Create
+        public ActionResult Create( HttpPostedFileBase image)
+        {
+            return View();
+        }
+
+        // POST: Producten/Create
+        [HttpPost]
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Create([Bind(Exclude = "Id")] Product product)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                _database.Product.Add(product);
+                _database.SaveChanges();
+                return RedirectToAction("Product");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Producten/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var ProductToEdit = (from m in _database.Product
+
+                where m.ProductID == id
+
+                select m).First();
+            return View(ProductToEdit);
+        }
+
+        // POST: Producten/Edit/5
+        [HttpPost]
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Edit(Product ProductToEdit)
+        {
+            var OriginalProduct = (from m in _database.Product
+                where m.ProductID == ProductToEdit.ProductID
+                select m).First();
+
+            if (!ModelState.IsValid)
+                return View(OriginalProduct);
+
+            _database.Entry(OriginalProduct).CurrentValues.SetValues(ProductToEdit);
+            _database.SaveChanges();
+
+            return RedirectToAction("Product");
+        }
+
+        // GET: Producten/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            Product Product = _database.Product.Find(id);
+            return View(Product);
+        }
+
+        // POST: Producten/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Product product = _database.Product.Find(id);
+            _database.Product.Remove(product);
+            _database.SaveChanges();
+            return RedirectToAction("Product");
+        }
+    }
+}
